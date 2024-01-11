83fd5f78780ab879b62ea2f0b206e0d628f233e2
Iyaz <moseseniade@gmail.com>
2024-01-10 20:42:50 -0700

    Add iteritems, items Py3

--- collatz.py
+++
@@ -1,8 +0,0 @@
-struct node
-{
-    int data;
-    str
-
-
-}
-
---
+++ iteritems.py
@@ -0,0 +1,6 @@
+# Python 3
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Using items()
+for key, value in my_dict.items():
+    print(key, value)
